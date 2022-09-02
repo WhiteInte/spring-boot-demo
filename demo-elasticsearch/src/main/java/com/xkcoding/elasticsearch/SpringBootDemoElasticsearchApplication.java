3ce6a9691c52e8258dff9442ce6fa97bf40d1976
xiaoyang <1152066671@qq.com>
2022-09-02 19:20:19 +0800

    git reset -1

--- demo-elasticsearch/src/main/java/com/xkcoding/elasticsearch/SpringBootDemoElasticsearchApplication.java
+++ demo-elasticsearch/src/main/java/com/xkcoding/elasticsearch/SpringBootDemoElasticsearchApplication.java
@@ -14,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoElasticsearchApplication {
 
+
+
+    //test
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoElasticsearchApplication.class, args);
     }
